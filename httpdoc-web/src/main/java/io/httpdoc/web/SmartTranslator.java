70ad8af4037bc74fc277c5b687a8add8e3b6cf38
YangChangPei <646742615@qq.com>
2018-04-24 11:01:14 +0800

    代码重构

--- httpdoc-web/src/main/java/io/httpdoc/web/SmartTranslator.java
+++ httpdoc-web/src/main/java/io/httpdoc/web/SmartTranslator.java
@@ -4,6 +4,7 @@ import io.httpdoc.core.Document;
 import io.httpdoc.core.Translation;
 import io.httpdoc.core.Translator;
 import io.httpdoc.core.exception.DocumentTranslationException;
+import org.qfox.detector.DefaultResourceDetector;
 
 /**
  * 缺省的翻译器
