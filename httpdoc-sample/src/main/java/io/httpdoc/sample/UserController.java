749b327524703d5340c6c9975054ce37147e17fd
杨昌沛 <646742615@qq.com>
2018-11-05 14:03:46 +0800

    1. 集成界面

---
+++ httpdoc-sample/src/main/java/io/httpdoc/sample/UserController.java
@@ -0,0 +1,17 @@
+package io.httpdoc.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 用户管理器
+ *
+ * @author 杨昌沛 646742615@qq.com
+ * 2018/11/5
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+}
