5b8d92e17ce5025b6011e9bb7374a897fe2bc766
core-lib <646742615@qq.com>
2018-11-18 17:13:45 +0800

    1. 采用SoftReference来引用文档对象，避免空闲时占用过多JVM内存。

---
+++ httpdoc-jestful-client/src/main/java/io/httpdoc/jestful/client/JestfulExporter.java
@@ -0,0 +1,43 @@
+package io.httpdoc.jestful.client;
+
+import io.httpdoc.core.Document;
+import io.httpdoc.core.export.BundleExporter;
+import io.httpdoc.core.export.Exporter;
+import io.httpdoc.core.generation.Generation;
+import io.httpdoc.jackson.deserialization.JsonDeserializer;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Jestful 导出器
+ */
+public class JestfulExporter extends BundleExporter implements Exporter {
+
+    @Override
+    public String platform() {
+        return "java";
+    }
+
+    @Override
+    public String framework() {
+        return "jestful";
+    }
+
+    @Override
+    public void export(String docURL, String folder) throws IOException {
+        // 将工程模板文件复制到该目录
+        copy("httpdoc-sdk/jestful-sdk", folder);
+
+        JestfulMergedGenerator generator = new JestfulMergedGenerator();
+
+        Document document = Document.from(new URL(docURL), new JsonDeserializer());
+        Generation generation = new Generation(document);
+        generation.setDirectory(folder + "/src/main/java");
+        generation.setSupplier(new JestfulSupplier());
+
+        generator.generate(generation);
+    }
+
+
+}
---
+++ httpdoc-jestful-client/src/main/resources/httpdoc-sdk/jestful-sdk/src/test/java/io/httpdoc/SDKGenerator.java
@@ -0,0 +1,38 @@
+package io.httpdoc;
+
+import io.httpdoc.core.Document;
+import io.httpdoc.core.generation.Generation;
+import io.httpdoc.jackson.deserialization.JsonDeserializer;
+import io.httpdoc.jestful.client.*;
+import org.junit.Test;
+
+import java.net.URL;
+
+public class SDKGenerator {
+
+    @Test
+    public void generate() throws Exception {
+        JestfulMergedGenerator generator = new JestfulMergedGenerator();
+//        generator.include(JestfulStandardGenerator.class);
+//        generator.include(JestfulCallbackGenerator.class);
+//        generator.include(JestfulEntityGenerator.class);
+//        generator.include(JestfulFutureGenerator.class);
+//        generator.include(JestfulGuavaGenerator.class);
+//        generator.include(JestfulHeaderGenerator.class);
+//        generator.include(JestfulJava8Generator.class);
+//        generator.include(JestfulLambdaGenerator.class);
+//        generator.include(JestfulMessageGenerator.class);
+//        generator.include(JestfulObservableGenerator.class);
+
+        Document document = Document.from(new URL("http://localhost:8080/httpdoc.json"), new JsonDeserializer());
+        Generation generation = new Generation(document);
+        generation.setDirectory(System.getProperty("user.dir") + "/src/main/java"); // set generate directory
+        generation.setSupplier(new JestfulSupplier());                             // set type supplier
+//        generation.setPkg();                                                      // set default package
+//        generation.setPkgForced();                                                // set forced package
+//        generation.setStrategy();                                                 // set generate strategy
+
+        generator.generate(generation);
+    }
+
+}
