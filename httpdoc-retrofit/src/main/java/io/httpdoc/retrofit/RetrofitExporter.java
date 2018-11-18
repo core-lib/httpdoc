f37812013dceafe66783d047fc86bfb16eaefb73
core-lib <646742615@qq.com>
2018-11-18 16:48:40 +0800

    1. 采用SoftReference来引用文档对象，避免空闲时占用过多JVM内存。

--- httpdoc-retrofit/src/main/java/io/httpdoc/retrofit/RetrofitExporter.java
+++ httpdoc-retrofit/src/main/java/io/httpdoc/retrofit/RetrofitExporter.java
@@ -1,24 +1,18 @@
 package io.httpdoc.retrofit;
 
-import io.detector.Resource;
-import io.detector.SimpleDetector;
 import io.httpdoc.core.Document;
+import io.httpdoc.core.export.BundleExporter;
 import io.httpdoc.core.export.Exporter;
 import io.httpdoc.core.generation.Generation;
-import io.httpdoc.core.kit.IOKit;
 import io.httpdoc.jackson.deserialization.JsonDeserializer;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.charset.Charset;
-import java.util.Collection;
 
 /**
  * Retrofit 导出器
  */
-public class RetrofitExporter implements Exporter {
+public class RetrofitExporter extends BundleExporter implements Exporter {
 
     @Override
     public String platform() {
@@ -33,28 +27,7 @@ public class RetrofitExporter implements Exporter {
     @Override
     public void export(String docURL, String folder) throws IOException {
         // 将工程模板文件复制到该目录
-        String bundle = "httpdoc-sdk/retrofit-sdk";
-        Collection<Resource> resources = SimpleDetector.Builder.scan(bundle)
-                .by(this.getClass().getClassLoader())
-                .includeJar()
-                .recursively()
-                .build()
-                .detect();
-        for (Resource resource : resources) {
-            try {
-                URL url = resource.getUrl();
-                String path = URLDecoder.decode(url.getPath(), Charset.defaultCharset().name());
-                int index = path.lastIndexOf(bundle);
-                String uri = path.substring(index + bundle.length());
-                File file = new File(folder, uri);
-                if (!file.getParentFile().exists() && !file.getParentFile().mkdirs()) {
-                    throw new IOException("could not make directory: " + file.getParentFile());
-                }
-                IOKit.transfer(resource.getInputStream(), file);
-            } finally {
-                IOKit.close(resource);
-            }
-        }
+        copy("httpdoc-sdk/retrofit-sdk", folder);
 
         RetrofitMergedGenerator generator = new RetrofitMergedGenerator();
 
@@ -65,5 +38,6 @@ public class RetrofitExporter implements Exporter {
 
         generator.generate(generation);
     }
+
 
 }
